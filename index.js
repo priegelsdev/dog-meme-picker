40b768000552543fe3ba10b90cb668778b941752
priegelsdev <priegelsdev@gmail.com>
2022-11-15 14:25:15 +0100

    pictures and create for of loop for emotions in index.js

---
+++ index.js
@@ -0,0 +1,145 @@
+const dogsData = [
+  {
+      emotionTags: ["confused", "moody"],
+      isGif: false,
+      image: "confused.jpeg",
+      alt: "A dog looking confused",
+  },
+  {
+      emotionTags: ["dominant"],
+      isGif: false,
+      image: "dominant.jpg",
+      alt: "A dog looking dominant",
+  },
+  {
+      emotionTags: ["happy"],
+      isGif: false,
+      image: "happy.jpg",
+      alt: "A dog looking happy",
+  },
+  {
+    emotionTags: ["hungry"],
+    isGif: false,
+    image: "hungry.jpg",
+    alt: "A dog looking hungry",
+  },
+  {
+    emotionTags: ["insomniac"],
+    isGif: false,
+    image: "insomniac1.jpg",
+    alt: "A dog looking sleepy",
+  },
+  {
+    emotionTags: ["insomniac"],
+    isGif: false,
+    image: "insomniac2.jpg",
+    alt: "A dog looking sleepy",
+  },
+{
+  emotionTags: ["moody", "dominant"],
+  isGif: false,
+  image: "moody1.jpg",
+  alt: "A dog looking moody",
+  },
+  {
+    emotionTags: ["moody"],
+    isGif: false,
+    image: "moody2.jpg",
+    alt: "A dog looking moody",
+  },
+  {
+    emotionTags: ["relaxed"],
+    isGif: false,
+    image: "relaxed.jpeg",
+    alt: "A dog looking relaxed",
+  },
+  {
+    emotionTags: ["sad"],
+    isGif: false,
+    image: "sad1.jpg",
+    alt: "A dog looking sad",
+  },
+  {
+    emotionTags: ["scared"],
+    isGif: false,
+    image: "scared.jpeg",
+    alt: "A dog looking scared",
+  },
+  {
+    emotionTags: ["confused"],
+    isGif: true,
+    image: "confused1.gif",
+    alt: "A dog looking confused",
+  },
+  {
+    emotionTags: ["confused"],
+    isGif: true,
+    image: "confused2.gif",
+    alt: "A dog looking confused",
+  },
+  {
+    emotionTags: ["dominant"],
+    isGif: true,
+    image: "dominant.gif",
+    alt: "A dog looking dominant",
+  },
+  {
+    emotionTags: ["happy"],
+    isGif: true,
+    image: "happy.gif",
+    alt: "A dog looking happy",
+  },
+  {
+    emotionTags: ["hungry", "dominant"],
+    isGif: true,
+    image: "hungry.gif",
+    alt: "A dog looking hungry",
+  },
+  {
+    emotionTags: ["insomniac"],
+    isGif: true,
+    image: "insomniac.gif",
+    alt: "A dog looking insomniac",
+  },
+  {
+    emotionTags: ["moody", "dominant"],
+    isGif: true,
+    image: "moody.gif",
+    alt: "A dog looking moody",
+  },
+  {
+    emotionTags: ["relaxed", "insomniac"],
+    isGif: true,
+    image: "relaxed.gif",
+    alt: "A dog looking relaxed",
+  },
+  {
+    emotionTags: ["sad"],
+    isGif: true,
+    image: "sad.gif",
+    alt: "A dog looking sad",
+  },
+  {
+    emotionTags: ["scared"],
+    isGif: true,
+    image: "scared.gif",
+    alt: "A dog looking scared",
+  },
+]
+
+// creating for of loop for emotion tags
+
+function getEmotionsArray(dogs) {
+  const emotionsArray = [];
+
+  for (let dog of dogs) {
+    for (let emotion of dog.emotionTags) {
+      emotionsArray.push(emotion);
+    }
+  }
+  console.log(emotionsArray)
+  let uniqueEmoArray = [...new Set(emotionsArray)];
+  console.log(uniqueEmoArray)
+}
+
+getEmotionsArray(dogsData);
