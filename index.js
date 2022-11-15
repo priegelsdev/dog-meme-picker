e1c095e32c767e46943b6e26e09f2479046c7479
priegelsdev <priegelsdev@gmail.com>
2022-11-15 19:28:13 +0100

    refactoring getEmotionsArray function

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const emotionsEl = document.querySelector('.emotion-radios');
 // create function to iterate over emotion array
 
 function getEmotionsArray(dogs) {
+
   const emotionsArray = [];
 
   for (let dog of dogs) {
     for (let emotion of dog.emotionTags) {
-      emotionsArray.push(emotion);
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      } 
     }
   }
-
-  let uniqueEmoArray = [...new Set(emotionsArray)];
-  return uniqueEmoArray;
+  return emotionsArray;
 }
 
 // create function to render out emotions from emotion array
